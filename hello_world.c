d683b1dd535975c3983ed35440b57b2ff6f003fc
GOROman <yoshihito.kondoh@gmail.com>
2024-06-04 10:22:14 +0900

    Add comments to source code

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,10 @@
+// This program prints "Hello World" to the console.
 #include <stdio.h>
 
+// Entry point of the program.
 int main() {
+    // Prints the message "Hello World" to the console.
     printf("Hello World\n");
+    // Indicates that the program ended successfully.
     return 0;
 }
